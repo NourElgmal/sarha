79858ee5b79cb9636c63926baee04c21ac5031c6
NourElgmal <nouralgmal123@gmail.com>
2024-07-21 20:02:36 +0300

    sarha app

--- service/photo.service.js
+++ service/photo.service.js
@@ -1,7 +1,7 @@
 const { photomodule } = require("../modules/photo.module");
 module.exports.addphoto=async(req,res)=>{
     const {title,path}=req.body
-    await photomodule.insertmeny({title,path})
+    await photomodule.insertMany({title,path})
     res.json({'msg':'add photo secc'});
 }
 module.exports.getphoto=async(req,res)=>{
